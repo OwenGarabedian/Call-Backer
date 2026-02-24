724c9efef0a2e07545dcd2f33d5d22aaf757f63e
Owen Garabedian <Owengarabedian@gmail.com>
2026-02-24 14:20:33 -0500

    New site map

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,25 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import sitemap from "vite-plugin-sitemap";
+
+const routes = [
+  "/",
+  "/Pricing",
+  "/sign-in",
+  "/sign-up",
+  "/support",
+  "/dashboard",
+  "/calls",
+  "/messages",
+  "/database",
+  "/control-center",
+  "/payment",
+  "/settings",
+  "/profileSettings",
+  "/automationConfig",
+  "/businessKnowledge",
+];
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -12,7 +31,14 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+    sitemap({
+      hostname: "https://www.call-backer.com",
+      dynamicRoutes: routes,
+    }),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
