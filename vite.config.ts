2fa111939e7eb8199924623d130456b0f4c20bad
Owen Garabedian <Owengarabedian@gmail.com>
2026-03-03 22:22:51 -0500

    New changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 import sitemap from "vite-plugin-sitemap";
 
 const routes = [
@@ -33,7 +32,6 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === "development" && componentTagger(),
     sitemap({
       hostname: "https://www.call-backer.com",
       dynamicRoutes: routes,
